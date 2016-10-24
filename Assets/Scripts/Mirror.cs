43e1ea27609c9cd1ab77880e30fcd84eb0a64e84
Alan Parsons <klaxorx@gmail.com>
2016-10-24 11:21:53 +0100

    Initial commit

---
+++ Assets/Scripts/Mirror.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mirror : MonoBehaviour {
+
+	public Camera mirrorCam;
+	public MeshRenderer mirrorRenderer;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mirrorRenderer.material.mainTexture = RTImage ();
+	}
+
+	Texture2D RTImage(){
+		RenderTexture currentRT = RenderTexture.active;
+		RenderTexture.active = mirrorCam.targetTexture;
+		mirrorCam.Render ();
+
+		Texture2D image = new Texture2D(mirrorCam.targetTexture.width, mirrorCam.targetTexture.height);
+		image.ReadPixels(new Rect(0, 0, mirrorCam.targetTexture.width, mirrorCam.targetTexture.height), 0, 0);
+		image.Apply();
+		RenderTexture.active = currentRT;
+		return image;
+	}
+}
