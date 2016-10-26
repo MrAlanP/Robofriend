2c76f410cb96b32c419fde72b24f3b56301fc14e
aparsons <alan@mobilepie.com>
2016-10-26 16:27:26 +0100

    - Volumetric lighting

--- Assets/Scripts/RobotPlayer.cs
+++ Assets/Scripts/RobotPlayer.cs
@@ -1,16 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using VRTK;
 
 public class RobotPlayer : MonoBehaviour {
 
+	public Light headLight;
+
+	public VRTK_ControllerEvents leftControllerEvents, rightControllerEvents;
+
 	// Use this for initialization
 	void Start () {
-	
+		leftControllerEvents.TouchpadPressed += new ControllerInteractionEventHandler(DoTouchpadClicked);
+		rightControllerEvents.TouchpadPressed += new ControllerInteractionEventHandler(DoTouchpadClicked);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//Debug key
+		if (Input.GetKeyDown (KeyCode.Q)) {
+			ToggleHeadLight ();
+		}
+	}
+
+	private void DoTouchpadClicked(object sender, ControllerInteractionEventArgs e)
+	{
+		ToggleHeadLight ();
 	}
 
 	/// <summary>
@@ -21,4 +35,9 @@ public class RobotPlayer : MonoBehaviour {
 		transform.position = teleportPoint;
 
 	}
+
+	void ToggleHeadLight(){
+		headLight.enabled = !headLight.enabled;
+	}
+
 }
